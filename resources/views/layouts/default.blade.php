641dd07ee07fc5702443412d5e4ee0769139f7af
To Ngo <tongo0209@gmail.com>
2021-01-07 14:02:29 +0700

    edit index cart

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -31,7 +31,7 @@
 <div class="col-12-nav">
     <div class="col-9">
         <div class="row row-nav"> 
-            <div class="col-1">
+            <div class="col-0.5">
                 <a class="navbar-brand" href="/">
                     <img class="img-nav"
                         src="https://upload.wikimedia.org/wikipedia/vi/archive/d/dc/20200125140746%21Vinfast-logo.png"
@@ -42,7 +42,7 @@
                 <div class="form-search-nav">
                     <form class="form-inline my-2 my-lg-0" action="/search" method="GET" role="form">
                         {{ csrf_field() }}
-                        <input class="form-control mr-sm-2" name="keyword" type="text" style="min-width: 25em;"
+                        <input class="form-control mr-sm-2" name="keyword" type="text" style="min-width: 15em;"
                             placeholder="Tìm kiếm sản phẩm">
                         <button class="btn btn-outline-none my-2 my-sm-0"
                         style="margin-left: -15px; background: #b2b2b2;  border: solid 1px #b2b2b2; "
