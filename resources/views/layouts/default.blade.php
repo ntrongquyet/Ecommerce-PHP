385084af37793c2f5d900328229b168d290fbb9d
ntrongquyet <ntrongquyet37@gmail.com>
2021-01-09 20:49:50 +0700

    edit link file js

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -107,7 +107,7 @@
 
     <!-- nhan -->
     <script src="{{ url('/js/likeProduct.js') }}"></script>
-    <script src="{{ url('dist/js/carousel.js') }}"></script>
+    <script src="{{ url('/JS/dist/carousel.js') }}"></script>
     <script src="{{ url('/js/scriptAdmin.js') }}"></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
     <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js'></script>
