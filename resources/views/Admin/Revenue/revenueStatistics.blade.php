72efe1649b9272332c6048cbe737e9bc2fd67559
MaiThanhNhan93 <mainhan.mtn93@gmail.com>
2021-01-09 11:39:01 +0700

    edit phan trang

--- resources/views/Admin/Revenue/revenueStatistics.blade.php
+++ resources/views/Admin/Revenue/revenueStatistics.blade.php
@@ -237,7 +237,7 @@
                             for (i = 0; i < numPages; i++) {
                                 var pageNum = i + 1;
                                 $('#nav').append(
-                                    '<li class="page-item"><a class="page-link" href="#" rel="' +
+                                    '<li class="page-item"><a class="page-link" rel="' +
                                     i + '">' + pageNum + '</a></li> ');
                             }
                             $('#data tbody tr').hide();
--- resources/views/frontend/Products/detailProduct.blade.php
+++ resources/views/frontend/Products/detailProduct.blade.php
@@ -159,7 +159,7 @@
                                 for (i = 0; i < numPages; i++) {
                                     var pageNum = i + 1;
                                     $('#nav').append(
-                                        '<li class="page-item"><a class="page-link" href="#" rel="' +
+                                        '<li class="page-item"><a class="page-link" rel="' +
                                         i + '">' + pageNum + '</a></li> ');
                                 }
                                 $('#data .media-body').hide();
@@ -191,45 +191,6 @@
                                     $('#likes').text(++like.value);
                                 }
                             }
-
-                            function pagination(){
-       var req_num_row=10;
-       var $tr=jQuery('tbody tr');
-       var total_num_row=$tr.length;
-       var num_pages=0;
-       if(total_num_row % req_num_row ==0){
-           num_pages=total_num_row / req_num_row;
-       }
-       if(total_num_row % req_num_row >=1){
-           num_pages=total_num_row / req_num_row;
-           num_pages++;
-           num_pages=Math.floor(num_pages++);
-       }
-       for(var i=1; i<=num_pages; i++){
-           jQuery('#pagination').append("<a href='#' class='btn'>"+i+"</a>");
-       }
-       $tr.each(function(i){
-           jQuery(this).hide();
-           if(i+1 <= req_num_row){
-               $tr.eq(i).show();
-           }
-
-       });
-       jQuery('#pagination a').click(function(e){
-           e.preventDefault();
-           $tr.hide();
-           var page=jQuery(this).text();
-           var temp=page-1;
-           var start=temp*req_num_row;
-           //alert(start);
-
-           for(var i=0; i< req_num_row; i++){
-
-               $tr.eq(start+i).show();
-
-           }
-       });
-}
 
                         </script>
                     @endsection
