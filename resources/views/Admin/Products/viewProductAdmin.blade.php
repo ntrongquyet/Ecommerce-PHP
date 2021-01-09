fac119fa8ebe33a011c5b1a4e54e14625985ca5a
MaiThanhNhan93 <mainhan.mtn93@gmail.com>
2021-01-09 17:03:17 +0700

    edit phan trang

--- resources/views/Admin/Products/viewProductAdmin.blade.php
+++ resources/views/Admin/Products/viewProductAdmin.blade.php
@@ -132,27 +132,31 @@ $(document).ready(function() {
         var rowsShown = 5;
         var rowsTotal = $('#data tbody tr').length;
         var numPages = rowsTotal / rowsShown;
-        for (i = 0; i < numPages; i++) {
-            var pageNum = i + 1;
-            $('#nav').append(
-                '<li class="page-item"><a class="page-link" rel="' +
-                i + '">' + pageNum + '</a></li> ');
+        if(numPages > 1)
+        {
+
+            for (i = 0; i < numPages; i++) {
+                var pageNum = i + 1;
+                $('#nav').append(
+                    '<li class="page-item"><a class="page-link" rel="' +
+                    i + '">' + pageNum + '</a></li> ');
+            }
+            $('#data tbody tr').hide();
+            $('#data tbody tr').slice(0, rowsShown).show();
+            $('#nav a:first').addClass('active');
+            $('#nav a').bind('click', function() {
+                $('#nav a').removeClass('active');
+                $(this).addClass('active');
+                var currPage = $(this).attr('rel');
+                var startItem = currPage * rowsShown;
+                var endItem = startItem + rowsShown;
+                $('#data tbody tr').css('opacity', '0.0').hide().slice(
+                    startItem, endItem).
+                css('display', 'table-row').animate({
+                    opacity: 1
+                }, 300);
+            });
         }
-        $('#data tbody tr').hide();
-        $('#data tbody tr').slice(0, rowsShown).show();
-        $('#nav a:first').addClass('active');
-        $('#nav a').bind('click', function() {
-            $('#nav a').removeClass('active');
-            $(this).addClass('active');
-            var currPage = $(this).attr('rel');
-            var startItem = currPage * rowsShown;
-            var endItem = startItem + rowsShown;
-            $('#data tbody tr').css('opacity', '0.0').hide().slice(
-                startItem, endItem).
-            css('display', 'table-row').animate({
-                opacity: 1
-            }, 300);
-        });
         //phân trang
 
 });
