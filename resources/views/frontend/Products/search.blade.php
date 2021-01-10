a9a3b1cc34ff4f79051a2db6299e3cdc1c2e75b3
MaiThanhNhan93 <mainhan.mtn93@gmail.com>
2021-01-10 14:35:17 +0700

    sua link anh

--- resources/views/frontend/Products/search.blade.php
+++ resources/views/frontend/Products/search.blade.php
@@ -11,28 +11,23 @@
     @else
     <div class="row justify-content-md-center"  id="search">
         @foreach($listProduct as $product)
-        <div class="col-md-4" id="img-search">
-            <figure class="card card-product-grid card-lg"> <a href="/{{$product->id_Cat}}/{{$product->id_product}}"
-                    class="img-wrap" data-abc="true">
-                    <img src="{{url('/image/products')}}/{{$product->avatar}}"></a>
+        <div class="col-md-4">
+            <figure class="card card-product-grid card-lg"> <a href="/{{$product->id_Cat}}/{{$product->id_product}}" class="img-wrap" data-abc="true">
+                    <img src="{{$product->avatar}}"></a>
                 <figcaption class="info-wrap">
                     <div class="row">
-                        <div class="col-md-9"> <a href="/{{$product->id_Cat}}/{{$product->id_product}}" class="title"
-                                data-toggle="tooltip" data-placement="bottom" title="{{$product->name}}"
-                                data-abc="true">{{$product->name}}</a> </div>
+                        <div class="col-md-9"> <a href="/{{$product->id_Cat}}/{{$product->id_product}}" class="title" data-toggle="tooltip" data-placement="bottom" title="{{$product->name}}" data-abc="true">{{$product->name}}</a> </div>
                         <div class="col-md-3 rating-edit">
                             <div class="rating text-right"> <i class="fas fa-heart"></i> {{$product->liked}}</div>
                         </div>
                     </div>
                 </figcaption>
                 <div class="bottom-wrap">
-
                     <div class="price-wrap"> <span class="price h5">{{number_format($product->price, 0, '', ',')}}
-                            VNĐ</span> <br> <small class="text-success">Free shipping</small> </div>
-                </div>
-                <a href="product/addToCart/{{$product->id_product}}" class="btn btn-primary float-right btn-price"
-                    data-abc="true"> Mua ngay
-                </a>
+                            VNĐ</span> <br> <small class="text-success">Free shipping</small>
+                        </div>
+                    </div>
+                      <p class="btn btn-primary float-right btn-price" data-key="{{$product->id_product}}" data-abc="true"> Mua ngay </p>
             </figure>
         </div>
         @endforeach
