cbb2692f64359050a0d98dfc1973918ae4f87066
ntrongquyet <ntrongquyet37@gmail.com>
2021-01-11 20:25:56 +0700

    nothing

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="{{ url('/style/carousel.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ url('/style/detailProductStyle.css') }}" />
